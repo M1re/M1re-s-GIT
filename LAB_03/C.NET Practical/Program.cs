7ca6f52434359a5fde3a90692fb112d8caddff77
M1re <71421857+M1re@users.noreply.github.com>
2020-11-27 17:08:14 +0200

    Add files via upload
    Вариант 11

---
+++ LAB_03/C.NET Practical/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace C.NET_Practical
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+
+            string Guess;
+            Console.WriteLine("Введите число для сравнения: ");
+
+            Guess = Console.ReadLine();
+
+            if (Guess.Length > 4|| Guess.Length<4)
+            {
+                Console.WriteLine("Вы ввели неправильное число!!!");
+                return -1;
+            }
+
+            if (Guess[0] == Guess[3])
+            {
+                if (Guess[1] == Guess[2])
+                {
+                    Console.WriteLine("Число читается как слева направо, так и справа налево");
+                    Console.WriteLine(Guess);
+                }
+            }
+            else
+                Console.WriteLine("Число не читается слева направо и справа налево");
+            return 0;
+
+			Console.ReadKey();
+		}
+    }
+}
