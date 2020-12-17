70a41bf668968ecee12dd946c13707e9ce0c8f1d
M1re <71421857+M1re@users.noreply.github.com>
2020-12-17 12:23:27 +0200

    Add files via upload

---
+++ LAB_08_Part 1/Program.cs
@@ -0,0 +1,131 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Garage
+    {
+        public string name;
+        public string color;
+        public int speed;
+        public int year;
+
+        public Garage(string _name, string _color, int _speed, int _year)
+        {
+            name = _name;
+            color = _color;
+            speed = _speed;
+            year = _year;
+        }
+        public void Print()
+        {
+            Console.WriteLine("Name " + name);
+            Console.WriteLine("Color " + color);
+            Console.WriteLine("Speed " + speed);
+            Console.WriteLine("Year " + year);
+
+        }
+    }
+
+    class Program
+    {
+        static void Menu()
+        {
+            Console.WriteLine("1. Купить машину");
+            Console.WriteLine("2. Продать машину");
+            Console.WriteLine("3. Выбрать машину");
+            Console.WriteLine("4. Покинуть гараж");
+        }
+        static void Main(string[] args)
+        {
+            List<Garage> auto = new List<Garage>();
+
+
+
+            while (true)
+            {
+                Console.WriteLine("Выберите пункт: ");
+                Menu();
+                int a = int.Parse(Console.ReadLine());
+                switch (a)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Введите марку: ");
+                            string name = Console.ReadLine();
+                            Console.WriteLine("Введите цвет: ");
+                            string color = Console.ReadLine();
+                            Console.WriteLine("Введите скорость: ");
+                            int speed = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Введите год выпуска: ");
+                            int year = int.Parse(Console.ReadLine());
+                            auto.Add(new Garage(name, color, speed, year));
+                            Console.WriteLine("Вы успешно купили новую машину!");
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Введите марку: ");
+                            string nameDel = Console.ReadLine();
+                            Console.WriteLine("Введите цвет: ");
+                            string colorDel = Console.ReadLine();
+
+                            for (int i = 0; i < auto.Count; i++)
+                            {
+                                if (nameDel == auto[i].name && colorDel == auto[i].color)
+                                    auto.RemoveAt(i);
+                            }
+
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Введите марку: ");
+                            string _name = Console.ReadLine();
+                            Console.WriteLine("Введите цвет: ");
+                            string _color = Console.ReadLine();
+                            Console.WriteLine("Введите скорость: ");
+                            int _speed = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Введите год выпуска: ");
+                            int _year = int.Parse(Console.ReadLine());
+
+                            for (int i = 0; i < auto.Count; i++)
+                            {
+                                if (_name == auto[i].name && _color == auto[i].color)
+                                    auto[i].Print();
+                                else if (_year == auto[i].year && _speed == auto[i].speed)
+                                    auto[i].Print();
+                                else Console.WriteLine("Подходящих машин нет!");
+                                break;
+                            }
+
+                            break;
+                        }
+                    case 4:
+                        {
+
+                            goto exit;
+                            
+                            break;
+                        }
+
+                    default:
+                        {
+                            Console.WriteLine("Введите существующий пункт меню!!!");
+                            break;
+                        }
+
+                }
+
+            }
+
+                      exit:
+               Console.ReadKey();
+
+        }
+    }
+}
---
+++ LAB_08_Part 2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB_08_2
+{
+    class DiskPhone
+    {
+        public string TelephoneNumber;
+        public static readonly string AllowdSymbols = "0123456789";
+
+        public void AnswerCall(string PhoneNumber) { }
+
+        public void MakeCall(string PhoneNumber) { }
+
+    }
+
+    class ButtonPhone : DiskPhone
+    {
+        new public static readonly string AllowdSymbols = "0123456789*#";
+
+        public void DisplayPhoneNumber(string PhoneNumber) { }
+        new public void AnswerCall(string PhoneNumber)
+        {
+            DisplayPhoneNumber(PhoneNumber);
+        }
+    }
+
+    class MobilePhone : ButtonPhone
+    {
+        new public static readonly string AllowdSymbols = "0123456789*#abcdefghijklmnopqrstuvwxyz+=-_()?:%;\"![]}{";
+        public string Resolution;
+        public string Size;
+        public string Color;
+
+        public void SendSMS(string PhoneNumber) { }
+
+        public void ReceiveSMS(string PhoneNumber) { }
+
+    }
+
+    class ColoredMobile : MobilePhone
+    {
+        public int DisplayColors;
+        public bool HasSecondSIM;
+        public string SecondNumber;
+        public void MakeCall(string PhoneNumber, bool UseSecondSIM) { }
+
+        public void SendSMS(string PhoneNumber, bool UseSecondSIM) { }
+
+        public void SendMMS(string PhoneNumber, bool UseSecondSIM) { }
+
+        public void ReceiveMMS(string PhoneNumber) { }
+    }
+
+    class SmartPhone : ColoredMobile
+    {
+        public bool HasSensor;
+        public int MaxTouches;
+        public int NumberOfCamers;
+
+        void MakePhoto(int CameraToUse) { }
+
+        void MakeVideo(int CameraToUse) { }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
