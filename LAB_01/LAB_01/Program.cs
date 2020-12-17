e04f34fb1dcd0333d2c670219b9b681d486ceaac
M1re <71421857+M1re@users.noreply.github.com>
2020-12-17 12:21:25 +0200

    Add files via upload

---
+++ LAB_01/LAB_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mori
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours;
+            int minutes;
+            int seconds;
+            seconds = int.Parse(Console.ReadLine());
+            hours = (int)(Math.Floor((double)(seconds / 3600)));
+            seconds = seconds % 3600;
+            minutes = (int)(Math.Floor((double)(seconds / 60)));
+            seconds = seconds % 60;
+            string time = hours + ":" + minutes + ":" + seconds;
+            Console.WriteLine(time);
+            Console.ReadKey();
+        }
+    }
+}
